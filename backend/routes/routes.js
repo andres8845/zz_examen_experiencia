60882c63e03e079867bf67269f1c67a386279258
AndresMartinez08 <andresmartinez_8845@hotmail.com>
2025-09-22 13:29:23 -0600

    ruta 1
    se agrego la primer ruta del get tours y las otras dos solo las deje ahi declaradas pero no hacen nada

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,4 +2,35 @@ const express = require("express");
 const router = express.Router(); 
 const {pool} = require('../config/db');
 
+router.get('/tours/:limit/:offset', async(req,res) => {
+    try{
+        const {limit = 10, offset = 0} = req.params;
+        const toures = await pool.query('select * from tourApp.tours limit $1 offset $2',[limit, offset]);
+        return res.json(toures.rows);
+    }catch(error){
+        console.error(error);
+        return res.status.json({mensaje : "Error 500 al querer retornar la lista de tours!"});
+    }
+    
+});
+
+router.get('/tours/availability', async(req,res) =>{
+    try{
+        const disponibles = 0 ;
+    }catch(error){
+
+    }
+});
+
+
+router.put('/tours/reserve', async(req,res)=> {
+    try{
+        const reservas = 0;
+    }catch(error){
+
+    }
+});
+
+
+
 module.exports = router;
